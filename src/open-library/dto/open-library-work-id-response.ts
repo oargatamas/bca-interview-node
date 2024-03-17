13f5e54b9d0c24da7ba96bd3d265162177bf6496
toarga <oarga.tamas@3dmap.hu>
2024-03-17 15:49:26 +0100

    OpenLibraryService fetch method implementation.

---
+++ src/open-library/dto/open-library-work-id-response.ts
@@ -0,0 +1,4 @@
+export class OpenLibraryWorkIdResponse{
+    workId: string;
+    publishedAt: Date;
+}
--- src/open-library/open-library-client.service.ts
+++ src/open-library/open-library-client.service.ts
@@ -1,10 +1,29 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { lastValueFrom } from 'rxjs';
+import {Injectable, InternalServerErrorException} from '@nestjs/common';
+import {lastValueFrom} from 'rxjs';
+import {OpenLibraryWorkIdResponse} from "./dto/open-library-work-id-response";
 
 @Injectable()
 export class OpenLibraryClientService {
   constructor(private readonly httpService: HttpService) {}
 
-  async getBookDetails(workId: string): Promise<any> {}
+  async getBookDetails(workId: string): Promise<OpenLibraryWorkIdResponse> {
+    try{
+      const apiResponse = await lastValueFrom(this.httpService.get(`https://openlibrary.org/works/${workId}.json`));
+      const data = apiResponse.data;
+
+      if(!data.first_publish_date){
+        throw new InternalServerErrorException("first_publish_date not existing in openlibrary.org response!");
+      }
+
+      const response = new OpenLibraryWorkIdResponse();
+      response.workId = workId;
+      response.publishedAt = new Date(data.first_publish_date);
+
+      return response;
+    }catch (error){
+      console.error("OpenLibraryClientService - Failed to fetch book data from openlibrary.org: ", error);
+      throw new InternalServerErrorException(error);
+    }
+  }
 }
